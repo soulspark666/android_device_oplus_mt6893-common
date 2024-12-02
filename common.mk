ae0b8f06e8f7e1672532f8ffc9ee7e05255d5354
ManshuTyagi <himanshut567@gmail.com>
2024-12-02 19:43:41 +0000

    mt6893-common: Nuke Dex/Art optimizations

--- common.mk
+++ common.mk
@@ -120,16 +120,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libcamera_metadata_shim
 
-# Dex/ART optimization
-PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
-PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := everything
-PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
-USE_DEX2OAT_DEBUG := false
-DONT_DEXPREOPT_PREBUILTS := true
-# Enable whole-program R8 Java optimizations for SystemUI and system_server
-SYSTEM_OPTIMIZE_JAVA := true
-SYSTEMUI_OPTIMIZE_JAVA := true
-
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0.vendor:64 \
