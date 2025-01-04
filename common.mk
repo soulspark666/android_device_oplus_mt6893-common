fb0505787632add82a79e0d25f4ce3292043bd05
Simao Gomes Viana <xdevs23@outlook.com>
2025-01-04 08:10:30 +0000

    mt6893-common: Set dalvik props for 8/12GB Variants
    * Values taken from https://github.com/AospExtended/platform_frameworks_native/commit/14a6b596477f6b688204101f0b1a8f040743e51c
    * Also stop inheriting 6GB dalvik-heap config
    
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: Ic9148527aa456da3993bb6449efe842a3e2973f9

--- common.mk
+++ common.mk
@@ -19,9 +19,6 @@ COMMON_PATH := device/oplus/mt6893-common
 # Installs gsi keys into ramdisk, to boot a GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
-# Dalvik
-$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
-
 PRODUCT_SHIPPING_API_LEVEL := 30
 
 # Call proprietary blob setup
